92f5e330bd08a8b2019f9c45865f28dca9d607b9
MohammadTawill <mohammad.tawil.01@gmail.com>
2025-05-12 22:38:52 +0300

    add css to the first page

--- resources/views/instagram_form.blade.php
+++ resources/views/instagram_form.blade.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Instagram Link to PDF</title>
 </head>
 <body>
-    <h1>Enter Instagram Link</h1>
+    
 
     <!-- Display success or error messages -->
     @if (session('success'))
@@ -18,8 +19,8 @@
 
     <form action="/generate-pdf" method="POST">
         @csrf
-        <label for="instagram_link">Instagram Link:</label>
-        <input type="url" id="instagram_link" name="instagram_link" required>
+        <label for="instagram_link" class="form-label">Enter Instagram Link:</label>
+        <input type="url" id="instagram_link" name="instagram_link" placeholder="https://instagram.com/username" required>
         <button type="submit">Generate PDF</button>
     </form>
 </body>
